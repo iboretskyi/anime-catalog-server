b7aadf1fd5fdff7eca1ca07d93135913cccc6a14
Ivan Boretskyi <boretskiy7.open1@gmail.com>
2023-05-22 00:22:10 +0300

    implemented stored procedures and user functions

--- src/controllers/website.controller.js
+++ src/controllers/website.controller.js
@@ -7,26 +7,18 @@ export const getHome = async (req, res, next) => {
   try {
     const pool = await getConnection();
     let result;
-    let query = '';
-    if (statusName) {
-      query = `SELECT ${limit ? `TOP (@limit)` : ''} Anime.id, Anime.title, Anime.description, Anime.score, Anime.imageUrl, Status.name AS status 
-                FROM Anime 
-                INNER JOIN Status ON Anime.statusId = Status.id 
-                WHERE Status.name = @statusName`;
-    } else {
-      query = `SELECT ${limit ? `TOP (@limit)` : ''} Anime.id, Anime.title, Anime.description, Anime.score, Anime.imageUrl, Status.name AS status 
-                FROM Anime 
-                INNER JOIN Status ON Anime.statusId = Status.id`;
-    }
+    let outputMessage = "";
     result = await pool.request()
-      .input('statusName', sql.VarChar, statusName)
       .input('limit', sql.Int, limit)
-      .query(query);
-    if (result.recordset.length === 0) {
+      .input('statusName', sql.VarChar, statusName)
+      .output('outputMessage', sql.NVarChar(255), outputMessage)
+      .execute('getHomeWithLimitAndStatus');
+
+    if (outputMessage === 'could not fetch from database') {
       throw new Error('could not fetch from database');
     }
-    const message = 'fetched anime from db successfully';
-    res.status(200).json({ message: message, animeList: result.recordset, status: statusName });
+
+    res.status(200).json({ message: outputMessage, animeList: result.recordset, status: statusName });
   } catch (err) {
     errorHandler(err, next);
   }
@@ -42,9 +34,8 @@ export const getEachAnime = async (req, res, next) => {
       .input('animeId', sql.Int, animeId)
       .query(`
         SELECT 
-          Anime.* 
-        FROM Anime 
-        WHERE Anime.id = @animeId
+          *
+        FROM dbo.getAnimeDetails(@animeId)
       `);
 
     if (!animeResult.recordset[0]) {
@@ -58,10 +49,8 @@ export const getEachAnime = async (req, res, next) => {
       .input('animeId', sql.Int, animeId)
       .query(`
         SELECT 
-          Genre.name AS genreName
-        FROM AnimeGenre 
-        LEFT JOIN Genre ON AnimeGenre.genreId = Genre.id
-        WHERE AnimeGenre.animeId = @animeId
+          *
+        FROM dbo.getAnimeGenres(@animeId)
       `);
 
     // Fetch reactions
@@ -69,10 +58,8 @@ export const getEachAnime = async (req, res, next) => {
       .input('animeId', sql.Int, animeId)
       .query(`
         SELECT 
-          Reaction.*, [User].username 
-        FROM Reaction 
-        INNER JOIN [User] ON Reaction.userId = [User].id
-        WHERE Reaction.animeId = @animeId
+          *
+        FROM dbo.getAnimeReactions(@animeId)
       `);
 
     const anime = animeResult.recordset[0];
@@ -113,57 +100,36 @@ export const getOtherUser = async (req, res, next) => {
     const pool = await getConnection();
 
     // Get user
-    const result = await pool.request()
+    const userResult = await pool.request()
       .input('userId', sql.Int, userId)
-      .query('SELECT * FROM [User] WHERE id = @userId');
-    const user = result.recordset[0];
+      .query('SELECT * FROM dbo.getUserById(@userId)');
+      
+    const user = userResult.recordset[0];
     if (!user) throw new Error('no user found');
 
     // Get followers
     const followers = await pool
       .request()
       .input('userId', sql.Int, userId)
-      .query(`
-        SELECT U.id, U.username 
-        FROM UserFollowers UF
-        INNER JOIN [User] U ON UF.followerId = U.id
-        WHERE UF.userId = @userId
-      `);
+      .query('SELECT * FROM dbo.getUserFollowers(@userId)');
 
     // Get followings
     const followings = await pool
       .request()
       .input('userId', sql.Int, userId)
-      .query(`
-        SELECT U.id, U.username 
-        FROM UserFollowing UF
-        INNER JOIN [User] U ON UF.followingId = U.id
-        WHERE UF.userId = @userId
-      `);
+      .query('SELECT * FROM dbo.getUserFollowings(@userId)');
 
     // Get reactions
     const reactions = await pool
       .request()
       .input('userId', sql.Int, userId)
-      .query(`
-        SELECT Reaction.*, Anime.title AS animeTitle, Anime.imageUrl AS animeImageUrl, [User].username AS reactedByUsername
-        FROM UserReactions 
-        INNER JOIN Reaction ON UserReactions.reactionId = Reaction.id
-        INNER JOIN Anime ON Reaction.animeId = Anime.id
-        INNER JOIN [User] ON Reaction.userId = [User].id
-        WHERE UserReactions.userId = @userId
-      `);
+      .query('SELECT * FROM dbo.getUserReactions(@userId)');
 
     // Get anime list
     const animeList = await pool
       .request()
       .input('userId', sql.Int, userId)
-      .query(`
-        SELECT AnimeList.*, Anime.title, Anime.imageUrl 
-        FROM AnimeList 
-        INNER JOIN Anime ON AnimeList.animeId = Anime.id 
-        WHERE AnimeList.userId = @userId
-      `);
+      .query('SELECT * FROM dbo.getUserAnimeList(@userId)');
 
     const message = 'fetched user successfully';
     res.status(200).json({
